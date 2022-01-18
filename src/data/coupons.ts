e9ff64b0461de01b51892b4d9cfb98568aec7d47
joelklingberg <joel.klingberg@hotmail.com>
2022-01-18 21:42:39 +0100

    CSS fixes and added first real coupon.

--- src/data/coupons.ts
+++ src/data/coupons.ts
@@ -3,21 +3,12 @@ import { Coupon } from "./models/Coupon.model";
 
 export const coupons: Coupon[] = [
     {
-        code: "HELLO2022",
-        description: "10% rabatt på hela sortimentet när du beställer valfria varor för minst 199 kr",
-        endDate: "2022-01-19",
-        link: "http://www.google.se",
-        logo: "https://www.adrecord.com/img/logos/954.gif",
-        startDate: "2022-01-16",
-        title: "10% rabatt"
-    },
-    {
-        code: "HELLO2022",
-        description: "10% rabatt på hela sortimentet när du beställer valfria varor för minst 199 kr",
-        endDate: "2022-01-18",
-        link: "http://www.google.se",
-        logo: "https://www.adrecord.com/img/logos/954.gif",
-        startDate: "2022-01-16",
-        title: "10% rabatt"
+        code: "valentines2022",
+        description: "14% rabatt på alla hjärtans dag presenter.",
+        startDate: "2022-01-17",
+        endDate: "2022-02-15",
+        link: "https://click.adrecord.com?c=50447&p=1107",
+        logo: "https://www.adrecord.com/img/logos/1107.gif",
+        title: "14% rabatt"
     },
 ]
